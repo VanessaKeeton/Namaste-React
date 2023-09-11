6a92507ff7514e1aecc5b5cd9f2e5f5c3aa01b3a
Vanessa Keeton <innerurge1@gmail.com>
2023-09-11 00:41:08 -0400

    ep two parcel and browser configs

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const heading = React.createElement("h1", { class: "heading1"}, "Hello world from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", { className: "heading1"}, "Hello world from React! This is cool.");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
